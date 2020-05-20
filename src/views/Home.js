e8fc8b0b0334af28da489436f729c5cf92df6e13
Oscar Mata <omata@applaudostudios.com>
2020-05-20 13:15:58 -0600

    :sparkles: updated project's structure

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,3 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Home extends Component {
-  render () {
-    return (
-      <div>Hello World</div>
-    )
-  }
-}
+export const Home = () => <h1>Hello there</h1>;
